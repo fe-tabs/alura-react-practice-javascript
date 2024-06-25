f84bb1aea0ab5f8b214bcbe177b3263747118d21
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-25 18:31:08 -0300

    :sparkles: feat: Title component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,14 @@
 import styles from './Home.module.css';
+import Title from 'components/Title';
 
 export default function Home() {
   return(
-    <></>
+    <>
+      <Title>
+        <h1>
+          Um lugar para guardar seus vídeos e filmes!
+        </h1>
+      </Title>
+    </>
   );
 }
