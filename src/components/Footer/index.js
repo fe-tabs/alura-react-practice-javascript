fb24786325b0700476d677adf4ab3ac960912329
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-25 20:59:12 -0300

    :bug: fix: position Footer at bottom

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return(
     <footer className={styles['footer']}>
-      <div className={styles['footer__container']}>
-        <p className={styles['footer__text']}>Desenvolvido com Alura</p>
-      </div>
+      <p className={styles['footer__text']}>Desenvolvido com Alura</p>
     </footer>
   )
 }
