7fd6a801e5cc1e008469b16726539be2a707ddaf
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-26 10:53:46 -0300

    :recycle: refactor: use nested routes

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,21 @@
-import Container from "components/Container";
-import Footer from "components/Footer";
-import Header from "components/Header";
-import FavoriteProvider from "contexts/FavoriteContext";
 import Favorite from "pages/Favorite";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
+import PageContent from "pages/PageContent";
 import Player from "pages/Player";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Header/>
-      
-      <Container>
-        <FavoriteProvider>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/favorite" element={<Favorite/>}/>
-            <Route path="/:id" element={<Player/>}/>
-            <Route path="*" element={<NotFound/>}/>
-          </Routes>
-        </FavoriteProvider>
-      </Container>
-
-      <Footer/>
+    <BrowserRouter>      
+      <Routes>
+        <Route path="/" element={<PageContent/>}>
+          <Route index element={<Home/>}/>
+          <Route path="favorite" element={<Favorite/>}/>
+          <Route path=":id" element={<Player/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
